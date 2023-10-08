ded75720bcec520b956244a728685eeef4c114f2
afyber <26539285+afyber@users.noreply.github.com>
2023-10-08 15:09:36 -0500

    Keyboard input

--- src/cursed.cpp
+++ src/cursed.cpp
@@ -1,5 +1,6 @@
 // cursed.cpp, entry point for the game
 #include <iostream>
+#include <string>
 #include <SDL.h>
 
 #include "libtcod.hpp"
@@ -16,20 +17,32 @@ int main(int argc, char* argv[]) {
 
 	tcod::ContextPtr context = tcod::new_context(params);
 
-	con.clear();
-
-	tcod::print(con, { 2, 3 }, "Hello world!", TCOD_white, std::nullopt);
-
-	context->present(con);
-
 	bool running = true;
+	std::string test = "";
 	while (running) {
+
+		con.clear();
+
+		tcod::print(con, { 0, 0 }, "A", TCOD_red, TCOD_white);
+		tcod::print(con, { 2, 3 }, "Hello world!", TCOD_red, std::nullopt);
+		tcod::print(con, { 29, 19 }, "G", TCOD_white, TCOD_blue);
+		tcod::print(con, { 2, 4 }, test, TCOD_white, std::nullopt);
+
+		context->present(con);
 
 		SDL_Event event;
 		while (SDL_PollEvent(&event) != NULL) {
 			// we have an event to handle
 			if (event.type == SDL_QUIT) {
 				running = false;
+			}
+			if (event.type == SDL_KEYDOWN) {
+				if (event.key.keysym.sym < 256 && std::isalnum(event.key.keysym.sym)) {
+					test += (char)event.key.keysym.sym;
+				}
+				if (event.key.keysym.sym == SDLK_RETURN) {
+					test = "";
+				}
 			}
 		}
 	}
