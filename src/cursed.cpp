cee1250be77c67d862584d75c05cae94e9607c9e
afyber <jeremydcraft.code@gmail.com>
2023-10-09 18:01:17 -0500

    Moved initialization logic into subroutines

--- src/cursed.cpp
+++ src/cursed.cpp
@@ -13,10 +13,8 @@ const int TILESET_CHARS = 16;
 const int CONSOLE_WIDTH = 20;
 const int CONSOLE_HEIGHT = 30;
 
-int main(int argc, char* argv[]) {
+void setup_tcod(tcod::Console& console_out, tcod::Context& context_out) {
 	auto params = TCOD_ContextParams();
-	params.argc = argc;
-	params.argv = argv;
 	params.tcod_version = TCOD_COMPILEDVERSION;
 	params.renderer_type = TCOD_RENDERER_SDL2;
 	params.vsync = 1;
@@ -25,22 +23,37 @@ int main(int argc, char* argv[]) {
 	tcod::Tileset tileset = tcod::load_tilesheet<>("data/tileset.png", { TILESET_CHARS, TILESET_CHARS }, tcod::CHARMAP_CP437);
 	params.tileset = tileset.get();
 
-	tcod::Console con = tcod::Console(CONSOLE_WIDTH, CONSOLE_HEIGHT);
-	params.console = con.get();
+	console_out = tcod::Console(CONSOLE_WIDTH, CONSOLE_HEIGHT);
+	params.console = console_out.get();
 
-	auto context = tcod::Context(params);
+	context_out = tcod::Context(params);
+}
 
+int setup_sdl(tcod::Context& context) {
 	if (IMG_Init(IMG_InitFlags::IMG_INIT_PNG) == 0) {
 		return -1;
 	}
 
 	SDL_SetWindowIcon(context.get_sdl_window(), IMG_Load("data/icon.png"));
+}
+
+void quit_sdl() {
+	IMG_Quit();
+}
+
+int main(int argc, char* argv[]) {
+	tcod::Console con;
+	tcod::Context context;
+	setup_tcod(con, context);
+
+	setup_sdl(context);
 
 	int scale_multiplier = 2;
 	SDL_SetWindowSize(context.get_sdl_window(), TILESET_CHAR_SIZE * CONSOLE_WIDTH * scale_multiplier, TILESET_CHAR_SIZE * CONSOLE_HEIGHT * scale_multiplier);
 
 	bool running = true;
 	std::string test = "";
+	int code = 0;
 	while (running) {
 
 		con.clear();
@@ -49,6 +62,7 @@ int main(int argc, char* argv[]) {
 		tcod::print(con, { 2, 3 }, "Hello world!", TCOD_ColorRGB{ 255, 0, 0 }, std::nullopt);
 		tcod::print(con, { 19, 29 }, "G", TCOD_ColorRGB{ 255, 255, 255 }, TCOD_ColorRGB{ 0, 0, 255 });
 		tcod::print(con, { 2, 4 }, test, TCOD_ColorRGB{ 255, 255, 255 }, std::nullopt);
+		tcod::print(con, { 2, 5 }, std::to_string(code), TCOD_ColorRGB{ 255, 255, 255 }, std::nullopt);
 
 		context.present(con);
 
@@ -73,11 +87,12 @@ int main(int argc, char* argv[]) {
 					}
 					test = "";
 				}
+				code = event.key.keysym.sym;
 			}
 		}
 	}
 
-	IMG_Quit();
+	quit_sdl();
 
 	return 0;
 }
