3930b3b7a3ea2b79dae51f59f188afefb156eac6
afyber <jeremydcraft.code@gmail.com>
2023-10-10 17:37:13 -0500

    Fixed up and refactored the keyboard code

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -2,60 +2,60 @@
 #include "keyboard.hpp"
 #include <SDL.h>
 
+/* The possible states of a key. All values greater than zero indicate the key is being held down, all values less than or equal to zero indicate it is not.
+ * -1: The key was released since the last update
+ * 0: The key is not being held down
+ * 1: The key has been pressed since the last update
+ * 2: The key has been typed again, but is still being held down (because holding down a key types it again repeatedly)
+ * 3: The key is being held down
+ */
 enum KEY_STATE {
 	JUST_RELEASED = -1,
-	NOT_DOWN = 0,
+	KEY_UP = 0,
 	JUST_PRESSED = 1,
 	RE_TYPED = 2,
 	KEY_DOWN = 3
 };
 
-/* Holds the state of every key on the keyboard as a number. All values greater than zero indicate the key is being held down, all values less than or equal to zero indicate it is not.
- * -1: The key was released since the last call to update_key_states()
- * 0: The key is not being held down
- * 1: Has been typed for the first time after not being held down
- * 2: The key has been typed again, but is still being held down
- * 3: They key is being held down
- */
-int8_t KEY_STATES[SDL_NUM_SCANCODES] = { 0 };
+KEY_STATE KEYS[SDL_NUM_SCANCODES] = { KEY_UP };
 
-bool key_down(SDL_Keycode key_scancode) {
-	return KEY_STATES[key_scancode] > 0;
+bool key_down(SDL_Scancode scancode) {
+	return KEYS[scancode] > 0;
 }
 
-bool key_pressed(SDL_Keycode key_scancode) {
-	return KEY_STATES[key_scancode] == JUST_PRESSED;
+bool key_pressed(SDL_Scancode scancode) {
+	return KEYS[scancode] == JUST_PRESSED;
 }
 
-bool key_typed(SDL_Keycode key_scancode) {
-	return KEY_STATES[key_scancode] == JUST_PRESSED || KEY_STATES[key_scancode] == RE_TYPED;
+bool key_typed(SDL_Scancode scancode) {
+	return KEYS[scancode] == JUST_PRESSED || KEYS[scancode] == RE_TYPED;
 }
 
-bool key_released(SDL_Keycode key_scancode) {
-	return KEY_STATES[key_scancode] == JUST_RELEASED;
+bool key_released(SDL_Scancode scancode) {
+	return KEYS[scancode] == JUST_RELEASED;
 }
 
 void update_key_states() {
 	for (int i = 0; i < SDL_NUM_SCANCODES; i++) {
-		if (KEY_STATES[i] == JUST_PRESSED || KEY_STATES[i] == RE_TYPED) {
-			KEY_STATES[i] = KEY_DOWN;
+		if (KEYS[i] == JUST_PRESSED || KEYS[i] == RE_TYPED) {
+			KEYS[i] = KEY_DOWN;
 		}
-		else if (KEY_STATES[i] == JUST_RELEASED) {
-			KEY_STATES[i] = NOT_DOWN;
+		else if (KEYS[i] == JUST_RELEASED) {
+			KEYS[i] = KEY_UP;
 		}
 	}
 }
 
 void handle_key_event(SDL_KeyboardEvent event) {
 	if (event.type == SDL_KEYDOWN) {
-		if (KEY_STATES[event.keysym.scancode] <= 0) {
-			KEY_STATES[event.keysym.scancode] = JUST_PRESSED;
+		if (KEYS[event.keysym.scancode] <= 0) {
+			KEYS[event.keysym.scancode] = JUST_PRESSED;
 		}
 		else {
-			KEY_STATES[event.keysym.scancode] = RE_TYPED;
+			KEYS[event.keysym.scancode] = RE_TYPED;
 		}
 	}
 	else if (event.type == SDL_KEYUP) {
-		KEY_STATES[event.keysym.scancode] = JUST_RELEASED;
+		KEYS[event.keysym.scancode] = JUST_RELEASED;
 	}
 }
--- src/keyboard.hpp
+++ src/keyboard.hpp
@@ -3,22 +3,22 @@
 #define CURSED_KEYBOARD_H_
 #include <SDL.h>
 
-bool key_pressed(SDL_Keycode key_scancode);
+bool key_down(SDL_Scancode scancode);
 
 /**
  * @returns true if the key was pressed since the last update, false otherwise
  */
-bool key_pressed(SDL_Keycode key_scancode);
+bool key_pressed(SDL_Scancode scancode);
 
 /**
  * @returns true if the key was typed since the last update, which will happen multiple times as a key is held down, false otherwise
  */
-bool key_typed(SDL_Keycode key_scancode);
+bool key_typed(SDL_Scancode scancode);
 
 /**
  * @returns true if the key was released since the last update, false otherwise
  */
-bool key_released(SDL_Keycode key_scancode);
+bool key_released(SDL_Scancode scancode);
 
 void update_key_states();
 
