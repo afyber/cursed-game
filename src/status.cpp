ee75149bff1e5b478050ed9c15d9e8f1d9f6770d
Jeremy Craft <jeremydcraft.code@gmail.com>
2023-12-10 20:58:40 -0600

    Finalized status system

--- src/status.cpp
+++ src/status.cpp
@@ -1,2 +1,102 @@
 // status.cpp, contains code for entity status effects and curses/blessings
 #include "status.hpp"
+
+// class Status
+
+void Status::set_defaults() {
+	set_effect(EFFECT_TYPE::DAMAGE_OFFSET, 0);
+	set_effect(EFFECT_TYPE::DAMAGE_MULTIPLIER, 1);
+	set_effect(EFFECT_TYPE::DAMAGE_LIMITER, -1);
+	set_effect(EFFECT_TYPE::HEALTH_OFFSET, 0);
+	set_effect(EFFECT_TYPE::HEALTH_LIMITER, -1);
+}
+
+void Status::set_effect(EFFECT_TYPE effect, double strength) {
+	effects[effect] = strength;
+}
+
+Status::Status(STATUS_TYPE type) {
+	set_defaults();
+	switch (type) {
+	case STATUS_TYPE::CURSE_ONE_HEALTH:
+		effects[EFFECT_TYPE::HEALTH_LIMITER] = 1;
+		break;
+	}
+}
+
+double Status::get_effect(EFFECT_TYPE effect) {
+	return effects[effect];
+}
+
+// class Temporary_Status
+
+Temporary_Status::Temporary_Status(STATUS_TYPE type, int actions) : Status(type), actions_left(actions) {}
+
+int Temporary_Status::get_actions() {
+	return actions_left;
+}
+
+void Temporary_Status::set_actions(int actions) {
+	actions_left = actions;
+}
+
+// class Entity_Status
+
+void Entity_Status::update(int actions) {
+	for (size_t i = 0; i < temporary_status.size(); ++i) {
+		Temporary_Status& t = temporary_status[i];
+		t.set_actions(t.get_actions() - actions);
+		if (t.get_actions() <= 0) {
+			temporary_status.erase(temporary_status.begin() + i);
+		}
+	}
+}
+
+double Entity_Status::get_effect(EFFECT_TYPE effect) {
+	double total;
+
+	// REMEMBER: every EFFECT_TYPE MUST have a case in this switch statement
+	// There are three types of effect:
+	switch (effect) {
+	case EFFECT_TYPE::DAMAGE_OFFSET:
+	case EFFECT_TYPE::HEALTH_OFFSET:
+		// additive
+		total = 0;
+		for (Temporary_Status s : temporary_status) {
+			total += s.get_effect(effect);
+		}
+		for (Status s : permanent_status) {
+			total += s.get_effect(effect);
+		}
+		break;
+	case EFFECT_TYPE::DAMAGE_MULTIPLIER:
+		// multiplicative
+		total = 1;
+		for (Temporary_Status s : temporary_status) {
+			total *= s.get_effect(effect);
+		}
+		for (Status s : permanent_status) {
+			total *= s.get_effect(effect);
+		}
+		break;
+	case EFFECT_TYPE::DAMAGE_LIMITER:
+	case EFFECT_TYPE::HEALTH_LIMITER:
+		// and limiting
+		total = -1;
+		double limit;
+		for (Temporary_Status s : temporary_status) {
+			if ((limit = s.get_effect(effect)) < total || total == -1) {
+				total = limit;
+			}
+		}
+		for (Status s : permanent_status) {
+			if ((limit = s.get_effect(effect)) < total || total == -1) {
+				total = limit;
+			}
+		}
+		break;
+	}
+
+	// if the above switch statement is written correctly there are no paths where total is uninitialized
+	return total;
+}
--- src/status.hpp
+++ src/status.hpp
@@ -1,24 +1,58 @@
 // status.hpp
 #ifndef CURSED_STATUS_HPP_
 #define CURSED_STATUS_HPP_
+#include <unordered_map>
 #include <vector>
 
+// REMEMBER: if you add an effect it also needs to be added to the switch statement in Entity_Status::get_effect(EFFECT_TYPE)
 enum class EFFECT_TYPE {
 	DAMAGE_OFFSET,
 	DAMAGE_MULTIPLIER,
 	DAMAGE_LIMITER,
 	HEALTH_OFFSET,
-	HEALTH_LIMITER,
-	NUM_EFFECTS
+	HEALTH_LIMITER
 };
 
-struct Effect {
-	EFFECT_TYPE type;
-	double value;
+enum class STATUS_TYPE {
+	CURSE_ONE_HEALTH
 };
 
 class Status {
-	std::vector<Effect> effects;
+private:
+	std::unordered_map<EFFECT_TYPE, double> effects;
+
+	void set_defaults();
+
+protected:
+	void set_effect(EFFECT_TYPE effect, double strength);
+
+public:
+	Status(STATUS_TYPE type);
+
+	double get_effect(EFFECT_TYPE effect);
+};
+
+class Temporary_Status : public Status {
+private:
+	int actions_left;
+
+public:
+	Temporary_Status(STATUS_TYPE type, int actions);
+
+	int get_actions();
+
+	void set_actions(int actions);
+};
+
+class Entity_Status {
+private:
+	std::vector<Status> permanent_status;
+	std::vector<Temporary_Status> temporary_status;
+
+public:
+	void update(int actions);
+
+	double get_effect(EFFECT_TYPE effect);
 };
 
 #endif
